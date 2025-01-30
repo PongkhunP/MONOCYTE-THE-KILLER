9767720f85694931773821fe2c74b9bc6d5c17c7
PongkhunP <139308094+PongkhunP@users.noreply.github.com>
2025-01-31 01:27:09 +0700

    Adding new player exploration mechanic and removing unneccesary file from the inventory & item directory

--- Assets/Scripts/Inventory/DisplayInventory.cs
+++ Assets/Scripts/Inventory/DisplayInventory.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Microsoft.Unity.VisualStudio.Editor;
 
 public class DisplayInventory : MonoBehaviour
 {
+    public GameObject inventoryPrefab;
     public InventoryObject inventory;
     public int X_START;
     public int Y_START;
@@ -12,49 +14,50 @@ public class DisplayInventory : MonoBehaviour
     public int NUMBER_OF_COLUMN;
     public int Y_SPACE_BETWEEN_ITEM;
     Dictionary<InventorySlot, GameObject> itemDisplayed = new Dictionary<InventorySlot, GameObject>();
-    // Start is called before the first frame update
-    void Start()
-    {
-        CreateDisplay();
-    }
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+    //     CreateDisplay();
+    // }
 
-    // Update is called once per frame
-    void Update()
-    {
-        UpdateDisplay();
-    }
+    // // Update is called once per frame
+    // void Update()
+    // {
+    //     UpdateDisplay();
+    // }
 
-    public void UpdateDisplay()
-    {
-        for (int i = 0; i < inventory.Container.Count; i++)
-        {
-            if (itemDisplayed.ContainsKey(inventory.Container[i]))
-            {
-                itemDisplayed[inventory.Container[i]].GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
-            }
-            else
-            {
-                var obj = Instantiate(inventory.Container[i].item.prefab, Vector3.zero, Quaternion.identity, transform);
-                obj.GetComponent<RectTransform>().localPosition = GetPosition(i);
-                obj.GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
-                itemDisplayed.Add(inventory.Container[i], obj);
-            }
-        }
-    }
+    // public void UpdateDisplay()
+    // {
+    //     for (int i = 0; i < inventory.Container.Count; i++)
+    //     {
+    //         if (itemDisplayed.ContainsKey(inventory.Container[i]))
+    //         {
+    //             itemDisplayed[inventory.Container[i]].GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
+    //         }
+    //         else
+    //         {
+    //             var obj = Instantiate(inventoryPrefab, Vector3.zero, Quaternion.identity, transform);
+    //             obj.transform.GetChild(0).GetComponentInChildren<Image>().sprite = ;
+    //             obj.GetComponent<RectTransform>().localPosition = GetPosition(i);
+    //             obj.GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
+    //             itemDisplayed.Add(inventory.Container[i], obj);
+    //         }
+    //     }
+    // }
 
-    public void CreateDisplay()
-    {
-        for (int i = 0; i < inventory.Container.Count; i++)
-        {
-            var obj = Instantiate(inventory.Container[i].item.prefab, Vector3.zero, Quaternion.identity, transform);
-            obj.GetComponent<RectTransform>().localPosition = GetPosition(i);
-            obj.GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
-            itemDisplayed.Add(inventory.Container[i], obj);
-        }
-    }
+    // public void CreateDisplay()
+    // {
+    //     for (int i = 0; i < inventory.Container.Count; i++)
+    //     {
+    //         var obj = Instantiate(inventory.Container[i].item.prefab, Vector3.zero, Quaternion.identity, transform);
+    //         obj.GetComponent<RectTransform>().localPosition = GetPosition(i);
+    //         obj.GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
+    //         itemDisplayed.Add(inventory.Container[i], obj);
+    //     }
+    // }
 
-    public Vector3 GetPosition(int i)
-    {
-        return new Vector3(X_START + (X_SPACE_BETWEEN_ITEM * (i % NUMBER_OF_COLUMN)), Y_START + (-Y_SPACE_BETWEEN_ITEM * (i / NUMBER_OF_COLUMN)), 0f);
-    }
+    // public Vector3 GetPosition(int i)
+    // {
+    //     return new Vector3(X_START + (X_SPACE_BETWEEN_ITEM * (i % NUMBER_OF_COLUMN)), Y_START + (-Y_SPACE_BETWEEN_ITEM * (i / NUMBER_OF_COLUMN)), 0f);
+    // }
 }
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -2,22 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : MonoBehaviour 
 {
     public InventoryObject inventory;
     
     public void OnTriggerEnter(Collider other)
     {
-        Item item = other.GetComponent<Item>();
+        GroundItem item = other.GetComponent<GroundItem>();
         if(item)
         {
-            inventory.AddItem(item.item, 1);
+            inventory.AddItem(new Item(item.item), 1);
             Destroy(other.gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            inventory.Save();
+            Debug.Log("Save Item successfully");
+        }
+        if(Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            inventory.Load();
+            Debug.Log("Load Item successfully");
         }
     }
 
     private void OnApplicationQuit()
     {
-        inventory.Container.Clear();
+        // inventory.Container.Clear();
     }
 }
